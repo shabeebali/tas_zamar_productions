76e4c72e7369f6d23c99e86caf0c18d608d82ffd
shabeeboali@gmail.com <shabeeboali@gmail.com>
2023-07-01 09:17:51 +0530

    menu link 2

--- app/Shortcodes/MenuLinkShortCode.php
+++ app/Shortcodes/MenuLinkShortCode.php
@@ -8,9 +8,9 @@ class MenuLinkShortCode
 {
     public function register($shortcode, $content, $compiler, $name, $viewData): string
     {
-        $baseClass = 'menu-item menu-item-type-post_type menu-item-object-page current-menu-item';
+        $baseClass = 'menu-item menu-item-type-post_type menu-item-object-page';
         if(URL::current() == URL::to($shortcode->to)) {
-            $baseClass.= ' jet-nav__item current_page_item';
+            $baseClass.= ' jet-nav__item current_page_item current-menu-item';
         }
         $str = '<div class="'.$baseClass.'">';
         $str .= '<a href="'.url($shortcode->to).'" class="menu-item-link menu-item-link-depth-0 menu-item-link-top">';
